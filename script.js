b902415998b9b620bd9532d32ec62f2fef998f71
sn-vuoch <srinnaravuoch@gmail.com>
2026-05-18 10:42:09 +0700

    Built getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello, world");
+function getComputerChoice() {
+  let computerRandomNumber = Math.random() * 10;
+  if (computerRandomNumber <= 3.33) {
+    return "rock";
+  } else if (computerRandomNumber <= 6.66) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+
